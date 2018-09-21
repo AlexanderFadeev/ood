ac134dc0588660c59a4dfe1450ab1123ae8707d4
AlexanderFadeev <1alexking1@gmail.com>
2018-09-21 14:46:03 +0300

    [Weather Observer] Add weather station implementation

---
+++ lab2/cmd/weather_station/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"ood/lab2/display"
+	"ood/lab2/stats_display"
+	"ood/lab2/weather_data"
+)
+
+func main() {
+	wd := weather_data.New()
+	d := display.New()
+	sd := stats_display.New()
+
+	conn := wd.Connect(d.Display, 5)
+
+	wd.SetTemperature(1)
+
+	statsConn := wd.Connect(sd.DisplayStats, 42)
+	defer statsConn.Close()
+
+	wd.SetPressure(2)
+	wd.SetHumidity(3)
+
+	conn.Close()
+
+	wd.SetValues(3, 14, 15)
+}
