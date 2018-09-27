1192e8a3537feda937772e545b58fc4cbdb7169f
AlexanderFadeev <1alexking1@gmail.com>
2018-09-27 21:24:53 +0300

    [Weather Observer] Add weather station pro duo

---
+++ lab2/cmd/weather_station_pro_duo/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"ood/lab2/display"
+	"ood/lab2/stats_display"
+	"ood/lab2/weather_data"
+)
+
+func main() {
+	in := weather_data.WeatherData(weather_data.New())
+	out := weather_data.New()
+
+	d := display.New()
+	sd := stats_display.New()
+
+	in.Connect(d.Display("In"), 1)
+	out.ConnectPro(d.DisplayPro("Out"), 1)
+	in.Connect(sd.DisplayStats("In"), 0)
+	out.ConnectPro(sd.DisplayStatsPro("Out"), 0)
+
+	in.SetTemperature(1)
+	in.SetPressure(2)
+	in.SetHumidity(3)
+	out.SetWind(100, 90)
+	out.SetWind(50, 180)
+	in.SetValues(3, 14, 15)
+}
