4ae953fe83d0c8dba08573534d64142ad2a7d33b
AlexanderFadeev <1alexking1@gmail.com>
2018-10-11 00:40:51 +0300

    [Editor] Add extra vector tests

--- lab5/vector/vector_test.go
+++ lab5/vector/vector_test.go
@@ -29,6 +29,14 @@ func TestVectorInsert(t *testing.T) {
 	assert.Equal(t, Vector{1, 2, 3, 4, 5}, vec)
 }
 
+func TestVectorInsertFront(t *testing.T) {
+	vec := Vector{2, 3, 4, 5}
+
+	vec.Insert(1, 0)
+
+	assert.Equal(t, Vector{1, 2, 3, 4, 5}, vec)
+}
+
 func TestVectorInsertBack(t *testing.T) {
 	vec := Vector{1, 2, 3, 4}
 
@@ -43,6 +51,22 @@ func TestVectorDelete(t *testing.T) {
 	vec.Delete(2)
 
 	assert.Equal(t, Vector{1, 2, 4, 5}, vec)
+}
+
+func TestVectorDeleteFirst(t *testing.T) {
+	vec := Vector{1, 2, 3, 4, 5}
+
+	vec.Delete(0)
+
+	assert.Equal(t, Vector{2, 3, 4, 5}, vec)
+}
+
+func TestVectorDeleteTheOnlyElement(t *testing.T) {
+	vec := Vector{1}
+
+	vec.Delete(0)
+
+	assert.Equal(t, Vector{}, vec)
 }
 
 func TestVectorDeleteBack(t *testing.T) {
@@ -60,4 +84,13 @@ func TestVectorPop(t *testing.T) {
 	assert.Equal(t, 5, val)
 
 	assert.Equal(t, Vector{1, 2, 3, 4}, vec)
+}
+
+func TestVectorPopTheOnlyElement(t *testing.T) {
+	vec := Vector{1}
+
+	val := vec.Pop()
+	assert.Equal(t, 1, val)
+
+	assert.Equal(t, Vector{}, vec)
 }
