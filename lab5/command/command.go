65831009f088451a427f47fdd18fd9463769e661
AlexanderFadeev <1alexking1@gmail.com>
2018-10-18 20:24:43 +0300

    [Editor] Add functional command wrapper

--- lab5/command/command.go
+++ lab5/command/command.go
@@ -5,3 +5,33 @@ type Command interface {
 	Unexecute() error
 	Release() error
 }
+
+type Callback func() error
+
+type command struct {
+	do, undo, release Callback
+}
+
+func noop() error {
+	return nil
+}
+
+func New(do, undo Callback) Command {
+	return WrapVerify(&command{do, undo, noop})
+}
+
+func NewWithRelease(do, undo, release Callback) Command {
+	return WrapVerify(&command{do, undo, release})
+}
+
+func (c *command) Execute() error {
+	return c.do()
+}
+
+func (c *command) Unexecute() error {
+	return c.undo()
+}
+
+func (c *command) Release() error {
+	return c.release()
+}
